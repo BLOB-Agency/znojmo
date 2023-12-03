cbbbd1cd82de41ba18d4134f4201ecdff57abd36
Guts6667 <76847062+Guts6667@users.noreply.github.com>
2023-12-03 04:24:46 +0100

    V.0.4
    Team ✅

--- src/app/[locale]/team/page.jsx
+++ src/app/[locale]/team/page.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import { useTranslations } from "next-intl";
+import players from "/public/datas/players.json";
+import TeamCard from "@/app/components/(Team)/TeamCard";
 export default function Team() {
   const t = useTranslations("Team");
+  console.log(players);
+  // let's fetch the datas from datas/player.json
+
   return (
     <main className="mt-[100px] p-[24px] md:p-[50px] text-black grid gap-[24px]">
-      <div className='flex flex-col md:flex-row gap-[24px] "w-fit whitespace-nowrap font-bold text-[25px]'>
+      <div className='flex flex-col md:flex-row gap-[24px] "w-fit font-bold text-[25px]'>
         <h2>{t("Title")}</h2>
         <p>{t("Description1")}</p>
       </div>
-      <section className="grid sm:grid-cols-2 md:grid-cols-3 lg-grid-cols-4">
-        {/* Map through team datas using TeamCard Component */}
-
+      <section className="grid sm:grid-cols-2 md:grid-cols-3 lg-grid-cols-4 2xl:flex 2xl:flex-wrap">
+        {players &&
+          players.map((player, index) => (
+            <TeamCard key={index} player={player} t={t} />
+          ))}
       </section>
     </main>
   );
--- src/app/components/(Team)/TeamCard/index.jsx
+++ src/app/components/(Team)/TeamCard/index.jsx
@@ -2,62 +2,71 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-export default function TeamCard({
-  socials,
-  firstName,
-  lastName,
-  age,
-  weight,
-  position,
-  number,
-  image,
-  t,
-}) {
+export default function TeamCard({ player, t }) {
+  console.log(player);
   return (
-    <article className="flex flex-col p-[24px]">
-      <div className="relative rounded-full w-[300px] h-[300px]">
+    <article className="relative flex flex-col items-center gap-[10px] p-[24px] min-w-[350px]">
+      <div className="relative rounded-full w-[200px] h-[200px] overflow-hidden">
         <Image
-          src={image}
-          alt={firstName}
+          src={`/images/players/${player.picture}`}
+          alt={`${player.firstName} ${player.lastName}`}
           fill
           priority
           sizes="100%"
-          className="object-fit"
+          className="object-cover h-full w-full "
         />
       </div>
-      <div>
-        <h3>
-          {firstName} <strong>{lastName}</strong>
+      <div className="grid grid-col gap-[10px] w-fit">
+        <h3 className="font-bold text-[24px] text-center">
+          {player.firstName} <strong>{player.lastName}</strong>
         </h3>
-        <div className="grid grid-cols-3">
-          <div>
-            <Image src="/icons/age.svg" alt={t("Age")} width={20} height={20} />
-            <span>{age}</span>
-          </div>
-          <div>
+        <div className="grid grid-cols-3 w-full justify-between">
+          <div className="flex gap-[10px] items-center justify-center">
             <Image
-              src="/icons/weight.svg"
-              alt={t("Age")}
+              src="/images/age.svg"
+              priority
+              alt={player.age}
               width={20}
               height={20}
             />
-            <span>{weight}</span>
+            <span>{player.age} {t("age")}</span>
           </div>
-          <div>
+          <div className="flex gap-[10px] items-center justify-center">
             <Image
-              src="/icons/number.svg"
+              src="/images/weight.svg"
+              alt={player.weight}
+              width={20}
+              height={20}
+              priority
+            />
+            <span>{player.weight} {t("weight")}</span>
+          </div>
+          <div className="flex gap-[10px] items-center justify-center">
+            <Image
+              src="/images/number.svg"
               alt={t("Age")}
               width={20}
               height={20}
+              priority
             />
-            <span>{number}</span>
+            <span>{t("number")} {player.number}</span>
           </div>
         </div>
       </div>
       <div className="absolute top-[24px] right-[24px] grid grid-col gap-[10px]">
-        {socials.map((social, index) => (
-          <Link href={social.link} key={index} className="transition-all duration-400 hover:scale-105">
-            <Image src={social.icon} alt={social.name} width={20} height={20} />
+        {player.socials.map((social, index) => (
+          <Link
+            href={social.link}
+            key={index}
+            className="transition-all duration-400 hover:scale-105"
+          >
+            <Image
+              src={"/images/" + social.icon}
+              alt={social.name}
+              priority
+              width={20}
+              height={20}
+            />
           </Link>
         ))}
       </div>
